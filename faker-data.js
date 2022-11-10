d969b8f16606dc281fccbc1900fd6dd4dc09d145
Brendan <bmall1019@gmail.com>
2022-11-09 22:38:48 -0500

    created + updated mack server

--- faker-data.js
+++ faker-data.js
@@ -1,5 +1,4 @@
 import { faker } from "@faker-js/faker";
-
 faker.seed(123);
 
 const generateUser = () => ({
@@ -12,8 +11,46 @@ const generateUser = () => ({
     registeredAt: faker.date.past(),
 });
 
-const user = generateUser();
+const USER = generateUser();
+
+const generatePlaylist = () => ({
+    playlistID: faker.datatype.uuid(),
+    image: faker.image.imageUrl(),
+    userId: faker.datatype.uuid(),
+    likes: Math.floor(Math.random() * 500),
+});
+
+const PLAYLISTS = [generatePlaylist(), generatePlaylist(), generatePlaylist()];
+
+const generateFriendActivity = () => ({
+    userId: faker.datatype.uuid(),
+    avatar: faker.internet.avatar(),
+    lastOnline: faker.date.past(),
+});
+
+const FRIENDSACTIVITY = [generateFriendActivity(), generateFriendActivity()];
+
+const generateActivity = () => ({
+    time: faker.date.past(),
+    type: faker.datatype.string(),
+    data: faker.datatype.json(),
+});
+
+const ACTIVITY = [
+    generateActivity(),
+    generateActivity(),
+    generateActivity(),
+    generateActivity(),
+];
+
+const SEARCHRESULT = {
+    results: faker.datatype.json(),
+};
 
 export const data = {
-    user: user,
+    USER,
+    PLAYLISTS,
+    FRIENDSACTIVITY,
+    ACTIVITY,
+    SEARCHRESULT,
 };
