34baea82043c3f236489c128c0db33892bfd26d1
Porter-House <will.porter17@gmail.com>
2022-11-17 14:12:56 -0500

    Update api.js

--- constants/api.js
+++ constants/api.js
@@ -6,3 +6,10 @@ export const API_PLAYLISTS = API + "/playlists";
 export const API_ACTIVITY_FRIENDS = API + "/activity/friends";
 export const API_ACTIVITY_ME = API + "/activity/me";
 export const API_SEARCH = API + "/search";
+
+//currentDoc = document.querySelector("FIlename.html")
+function changeUsername(){}
+function changeFirst(){}
+function changeLast(){}
+function changeEmail(){}
+function changePassword(){}
