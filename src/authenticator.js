5887641331af58659591dd2aacc0e460a44d253d
izaias-cunha <icunha@umass.edu>
2022-11-19 15:57:31 -0500

    auth 3

--- src/authenticator.js
+++ src/authenticator.js
@@ -1,35 +1,71 @@
-//**Figure out what to do if user does not authenticate**\\
-
-const client_id = '84f9f6cbc26d428ea9b95b1ca65861ad';
-const redirect_uri = 'https://spotlist.herokuapp.com/callback';
+//figure out what buffer does\\
 
 import express, { response } from 'express';
 import queryString from 'node:querystring';
+import axios from 'axios';
+
+require('dotenv').config();
 
 const app = express();
 
-//Request User Authorization
-app.get('/signup/auth', function(req, res) {
+let secret, CLIENT_ID, CLIENT_SECRET, URI;
 
-    //find out if this is a built in fuction
-    const state = generateRandomString(16);
-    const scope = 'user-modify-playback-state playlist-modify-public streaming';
+if(!process.env.CLIENT_ID) {
+    secret = require('secret.JSON');
+    CLIENT_ID = secret.CLIENT_ID;
+    CLIENT_SECRET = secret.CLIENT_SECRET;
+    URI = secret.URI;
+} else {
+    CLIENT_ID = process.env.CLIENT_ID;
+    CLIENT_SECRET = process.env.CLIENT_SECRET;
+    URI = process.env.URI;
+}
+
+//Request User Authorization
+app.get('/auth', function(req, res) {
+
+    const scope = 'user-modify-playback-state playlist-modify-public streaming  user-read-private';
 
     res.redirect('https://accounts.spotify.com/authorize?' + 
         queryString.stringify({
             response_type: 'code',
-            client_id: client_id,
+            client_id: CLIENT_ID,
             scope: scope,
-            redirect_uri: redirect_uri,
-            state: state
+            redirect_uri: URI,
         })
     );
 });
 
 //Request Access Token
-app.get('/callback', function(req, res) {
-    const code = req.query.code;
-    const state = req.query.state;
+app.get('/auth/callback', function(req, res) {
+    const code = req.query.code || null;
 
-    if ()
+    if (code === null) {
+        res.status(400).send(req.query.error);
+    }
+
+    axios({
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        data: queryString.stringify({
+            grant_type: 'authorization_code',
+            code: code,
+        }), 
+        header : {
+            'content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+        }
+    })
+        .then(response => {
+            if (response.status === 200) {
+                //send to db
+                window.localStorage('token', JSON.stringify(response.data))
+                res.send();
+            } else {
+                res.status(response.staus).send('Token Error');
+            }
+        })
+        .catch(error => {
+            res.status(400).send(error);
+        });  
 });
