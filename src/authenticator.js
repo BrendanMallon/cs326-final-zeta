42ccad8ecc4e0d0c4898b954178ff7fa306e4f2c
Brendan <bmall1019@gmail.com>
2022-11-19 16:19:00 -0500

    fixing errors

--- src/authenticator.js
+++ src/authenticator.js
@@ -1,17 +1,17 @@
 //figure out what buffer does\\
 
-import express, { response } from 'express';
-import queryString from 'node:querystring';
-import axios from 'axios';
+import express, { response } from "express";
+import queryString from "node:querystring";
+import axios from "axios";
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
 let secret, CLIENT_ID, CLIENT_SECRET, URI;
 
-if(!process.env.CLIENT_ID) {
-    secret = require('secret.JSON');
+if (!process.env.CLIENT_ID) {
+    secret = require("secret.JSON");
     CLIENT_ID = secret.CLIENT_ID;
     CLIENT_SECRET = secret.CLIENT_SECRET;
     URI = secret.URI;
@@ -26,28 +26,28 @@ app.get("/auth", function (req, res) {
     const scope =
         "user-modify-playback-state playlist-modify-public streaming  user-read-private";
 
-    const scope = 'user-modify-playback-state playlist-modify-public streaming  user-read-private';
-
-    res.redirect('https://accounts.spotify.com/authorize?' + 
-        queryString.stringify({
-            response_type: 'code',
-            client_id: CLIENT_ID,
-            scope: scope,
-            redirect_uri: URI,
-        })
+    res.redirect(
+        "https://accounts.spotify.com/authorize?" +
+            queryString.stringify({
+                response_type: "code",
+                client_id: CLIENT_ID,
+                scope: scope,
+                redirect_uri: URI,
+            })
     );
 });
 
 //Request Access Token
-app.get('/auth/callback', function(req, res) {
+app.get("/auth/callback", function (req, res) {
     const code = req.query.code || null;
 
     if (code === null) {
         res.status(400).send(req.query.error);
     }
+});
 
 app.get("/refresh_token", function (req, res) {
-    var refresh_token = req.query.refresh_token;
+    var { refresh_token } = req.query;
     var authOptions = {
         url: "https://accounts.spotify.com/api/token",
         headers: {
@@ -62,9 +62,9 @@ app.get("/refresh_token", function (req, res) {
         json: true,
     };
 
-    request.post(authOptions, function (error, response, body) {
+    req.post(authOptions, function (error, response, body) {
         if (!error && response.statusCode === 200) {
-            var access_token = body.access_token;
+            var { access_token } = body;
             res.send({
                 access_token: access_token,
             });
