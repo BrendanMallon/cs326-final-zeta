cdc0257a27419624b99d23da8a5e0ea723d6da0a
izaias-cunha <icunha@umass.edu>
2022-12-02 12:45:11 -0500

    finished

--- src/loginPage.js
+++ src/loginPage.js
@@ -1,3 +1,22 @@
-function signIn(){
+const bcrypt = require('bcrypt');
+import mdbGetUserInfo from "./mongoDB.js"
 
+async function signIn(){
+    const username = document.getElementById("signInUsername"),
+        password = document.getElementById("signInPassword"),
+        hash = "";
+
+    const userInfo = mdbGetUserInfo(username);
+    if (userInfo !== -1)
+        try {
+            //password needs to be changed
+            if (await bcrypt.compare(password, userInfo)) {
+                //succsesful login
+            } else {
+                alert("Incorrect username or password");
+            }
+        } catch (e) {
+    } else {
+        alert("Incorrect username or password");
+    }
 }
--- src/signupPage.js
+++ src/signupPage.js
@@ -1,14 +1,18 @@
 import {mdbGetUserEmail, mdbGetUserInfo, mdbAddUser} from './mongoDB.js'
+const bcrypt = require('bcrypt');
+
 //signUp function called from signup page to create a new user
-function signUp(){
-    newPWord = document.getElementById("signUpPassword").value
-    newEmail = document.getElementById("signUpEmail").value
-    newUserName = document.getElementById("signUpUsername").value
-    newConfirmPWord = document.getElementById("signUpConfirmPassword").value
-    newName = document.getElementById("signupName").value
+async function signUp(){
+    const newPWord = document.getElementById("signUpPassword").value,
+        newEmail = document.getElementById("signUpEmail").value,
+        newUserName = document.getElementById("signUpUsername").value,
+        newConfirmPWord = document.getElementById("signUpConfirmPassword").value,
+        newName = document.getElementById("signupName").value;
+
+    const error = "";
     if(!(newPWord === newConfirmPWord)){
         //ERROR PASSWORDS DONT MATCH, PRINT ERROR ON SIGNUP PAGE
-        return null
+        error += "Passwords do not match\n";
     }
     existingUser, existingEmail = mdbGetUserEmail(newUserName, newEmail).catch(console.dir)
     if(returnedEmail === -1 || returnedUser === -1){
@@ -17,15 +21,23 @@ function signUp(){
     }
     else if(existingUser === null && existingEmail === null){
         //user account created succesfully
-        mdbAddUser(newUserName, newEmail, newPWord, newName)
-
+        try {
+            const salt = await bcrypt.genSalt(10);
+            const hashedPassword = await bcrypt.hash(newPWord, salt);
+            mdbAddUser(newUserName, newEmail, hashedPassword, newName)
+        } catch (e) {
+            console.log(e);
+        }
     }else if(!(existingUser === null)){
         //ERROR ACCOUNT WITH THIS USERNAME ALREADY EXISTS
-        return null
+        error += "User already exists. Enter new username\n";
     }else if(!(existingEmail === null)){
         //ERROR ACCOUNT WITH THIS EMAIL ALREADY EXISTS
-        return null
+        error += "Email already in use. Enter new email\n";
     }
     
+    if (error !== "") {        
+        alert(error);
+    }
 }
 
