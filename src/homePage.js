95a7479294c1ef899caa6de54d05dc0d18a4eb08
izaias-cunha <icunha@umass.edu>
2022-12-10 15:14:56 -0500

    spotify player

--- src/homePage.js
+++ src/homePage.js
@@ -1,34 +1,62 @@
-import {getPlaylist, followPlaylist} from './spotify.js';
+import {checkToken, getPlaylist, followPlaylist} from './spotify.js';
 
 let playlist = [];
-let currPlaylist = {};
-let index = 0;
-let query = "";
+const devid = '';
+let indx = 0;
 
 document.getElementById("submit").addEventListener("click", () => {
     query = document.getElementById("submit").value;
 
     playlist = getPlaylist(query, 0);
-    currPlaylist = playlist[index];
-    ++index;
+    loadTracks();
 });
 
-function likePlaylist(){
-    followPlaylist(currPlaylist.id);
-    //add to mongo
-    if (index === 20) {
-        playlist = getPlaylist(query, index);
-        index = 0;
-    } else {
-        currPlaylist = playlist[++index];
-    }
+window.onSpotifyWebPlaybackSDKReady = () => {
+    checkToken();
+    const token = JSON.parse(window.localStorage('token')).access_token;
+    const player = new Spotify.Player({
+        name: 'spotlist',
+        getOAuthToken: cb => { cb(token); },
+        volume: 0.5
+    });
+
+    player.connect().then(success => {
+        if (success) {
+            console.log('The Web Playback SDK successfully connected to Spotify!');
+        }
+    })
+
+    player.addListener('ready', ({ device_id }) => {
+        console.log('The Web Playback SDK is ready to play music!');
+        devid = device_id;
+        console.log('Device ID', device_id);
+    })
 }
 
-function dislikePlaylist(){
-    if (index === 20) {
-        playlist = getPlaylist(query, index);
-        index = 0;
-    } else {
-        currPlaylist = playlist[++index];
+function loadTracks() {
+    checkToken();
+    const token = JSON.parse(window.localStorage.getItem(token)).access_token;
+
+    if (indx === playlist.length) {
+        window.alert("please search for new playlists");
+        return;
     }
+
+    const currPlaylist = playlist[indx].tracks.items;
+    ++indx;
+
+    fetch(`https://api.spotify.com/v1/me/player/play?device_id=${devid}`, {
+        method: 'PUT',
+        data: {context_uri : `spotify:playlist:${currPlaylist.uri}`},
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        }
+    });
+}
+
+function likePlaylist(){
+    followPlaylist(playlist.id);
+    //add to mongo
+    loadTracks();
 }
