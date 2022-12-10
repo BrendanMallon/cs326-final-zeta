6395c9c6053a428bd46bfe0bc4a042165139daeb
Brendan <bmall1019@gmail.com>
2022-12-10 15:29:54 -0500

    mongodb connection

--- constants/api.js
+++ constants/api.js
@@ -1,11 +1,6 @@
 const API = "/api";
 
-export const API_LOGIN = API + "/login";
-export const API_REGISTER = API + "/register";
-export const API_PLAYLISTS = API + "/playlists";
-export const API_ACTIVITY_FRIENDS = API + "/activity/friends";
-export const API_ACTIVITY_ME = API + "/activity/me";
-export const API_SEARCH = API + "/search";
+export const API_FIND_USER = API + "/findUser";
+export const API_REGISTER_USER = API + "/registerUser";
 
 //currentDoc = document.querySelector("FIlename.html")
-
--- src/homePage.js
+++ src/homePage.js
@@ -1,34 +1,59 @@
-import {getPlaylist, followPlaylist} from './spotify.js';
+function makeApiCall(event) {
+    // Prevent the form from being submitted
+    event.preventDefault();
+    console.log("test");
+    // Get the value of the input field
+    var input = document.getElementById("input-field").value;
 
-let playlist = [];
-let currPlaylist = {};
-let index = 0;
-let query = "";
-
-document.getElementById("submit").addEventListener("click", () => {
-    query = document.getElementById("submit").value;
-
-    playlist = getPlaylist(query, 0);
-    currPlaylist = playlist[index];
-    ++index;
-});
-
-function likePlaylist(){
-    followPlaylist(currPlaylist.id);
-    //add to mongo
-    if (index === 20) {
-        playlist = getPlaylist(query, index);
-        index = 0;
-    } else {
-        currPlaylist = playlist[++index];
-    }
+    // Use Axios to make the API call
+    fetch(window.location.origin + "/api/findUser", {
+        method: "GET",
+        params: {
+            username: input,
+        },
+    })
+        .then(function (response) {
+            // Handle the response here
+            console.log(response);
+        })
+        .catch(function (error) {
+            // Handle the error here
+        });
+    return false;
 }
+document.getElementById("foo").addEventListener("click", makeApiCall);
+console.log("TEST");
+// import {getPlaylist, followPlaylist} from './spotify.js';
 
-function dislikePlaylist(){
-    if (index === 20) {
-        playlist = getPlaylist(query, index);
-        index = 0;
-    } else {
-        currPlaylist = playlist[++index];
-    }
-}
+// let playlist = [];
+// let currPlaylist = {};
+// let index = 0;
+// let query = "";
+
+// document.getElementById("submit").addEventListener("click", () => {
+//     query = document.getElementById("submit").value;
+
+//     playlist = getPlaylist(query, 0);
+//     currPlaylist = playlist[index];
+//     ++index;
+// });
+
+// function likePlaylist(){
+//     followPlaylist(currPlaylist.id);
+//     //add to mongo
+//     if (index === 20) {
+//         playlist = getPlaylist(query, index);
+//         index = 0;
+//     } else {
+//         currPlaylist = playlist[++index];
+//     }
+// }
+
+// function dislikePlaylist(){
+//     if (index === 20) {
+//         playlist = getPlaylist(query, index);
+//         index = 0;
+//     } else {
+//         currPlaylist = playlist[++index];
+//     }
+// }
